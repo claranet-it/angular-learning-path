67a16994a1ba112bd01d4b1878628ab4787f17d2
Daniele Luconi <daniele.luconi@claranet.com>
2025-01-07 17:17:25 +0100

    sailor comments test

--- src/app/components/sailor-comments/sailor-comments.component.spec.ts
+++ src/app/components/sailor-comments/sailor-comments.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { of } from 'rxjs';
 
 import { SailorCommentsComponent } from './sailor-comments.component';
 import { SailorCommentsService } from '../../services/sailor-comments.service';
@@ -9,12 +8,8 @@ import { SailorCommentsService } from '../../services/sailor-comments.service';
 describe('SailorCommentsComponent', () => {
   let component: SailorCommentsComponent;
   let fixture: ComponentFixture<SailorCommentsComponent>;
-  let sailorCommentsService: SailorCommentsService;
 
   beforeEach(async () => {
-    sailorCommentsService = jasmine.createSpyObj('SailorCommentsService', ['fetchSailorComments']);
-    (sailorCommentsService.fetchSailorComments as jasmine.Spy).and.returnValue(of([]));
-
     await TestBed.configureTestingModule({
       providers: [
         provideHttpClient(),
@@ -27,10 +22,21 @@ describe('SailorCommentsComponent', () => {
     fixture = TestBed.createComponent(SailorCommentsComponent);
     component = fixture.componentInstance;
     fixture.componentRef.setInput('sailorId', '3');
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have sailorId input set', () => {
+    expect(component.sailorId()).toBe('3');
+  });
+
+  it('should call fetchSailorComments with correct sailorId', () => {
+    const sailorCommentsService = TestBed.inject(SailorCommentsService);
+    const spy = spyOn(sailorCommentsService, 'fetchSailorComments');
+    fixture.componentRef.setInput('sailorId', '4');
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith('4');
   });
 });
