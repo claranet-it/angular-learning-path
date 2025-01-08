cff568e52445293c9e8ee230fe83b98d73885993
Daniele Luconi <daniele.luconi@claranet.com>
2025-01-08 16:04:10 +0100

    sailors list component test

--- src/app/components/sailors-list/sailors-list.component.spec.ts
+++ src/app/components/sailors-list/sailors-list.component.spec.ts
@@ -1,20 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { SailorsListComponent } from './sailors-list.component';
+import { SailorService } from '../../services/sailor.service';
+import { Sailor } from '../../models/sailor';
 
 describe('SailorsListComponent', () => {
   let component: SailorsListComponent;
   let fixture: ComponentFixture<SailorsListComponent>;
+  let sailorService: jasmine.SpyObj<SailorService>;
 
   beforeEach(async () => {
+    sailorService = jasmine.createSpyObj('SailorService', ['getSailors', 'deleteSailor', 'resources']);
+    sailorService.getSailors.and.returnValue(of([]));
+    sailorService.deleteSailor.and.returnValue(of({} as Sailor));
+
     await TestBed.configureTestingModule({
+      imports: [SailorsListComponent],
       providers: [
         provideHttpClient(),
-        provideHttpClientTesting()
-      ],
-      imports: [SailorsListComponent]
+        provideHttpClientTesting(),
+        { provide: SailorService, useValue: sailorService }
+      ]
     })
     .compileComponents();
 
@@ -26,4 +35,15 @@ describe('SailorsListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call getSailors on initialization', () => {
+    expect(sailorService.getSailors).toHaveBeenCalled();
+  });
+
+  it('should call deleteSailor with the correct ID', () => {
+    const sailorId = '1';
+    component.deleteSailor(sailorId);
+    expect(sailorService.deleteSailor).toHaveBeenCalledWith(sailorId);
+  });
+
 });
